7569a05e345ea3b1bee8ec8b3a1037e2a19b8e31
Douglas Allen <kb9agt@gmail.com>
2015-10-17 23:54:07 -0500

    added configure to archive

--- ext/nokogiri/tmp/i686-pc-linux-gnu/ports/libxml2/2.9.2/libxml2-2.9.2/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/usr/local/lib/ruby/gems/2.2.0/gems/nokogiri-1.6.6.2/ports/i686-pc-linux-gnu/libxml2/2.9.2/lib"
-XML2_LIBS="-lxml2 -lz   -lm "
-XML2_INCLUDEDIR="-I/usr/local/lib/ruby/gems/2.2.0/gems/nokogiri-1.6.6.2/ports/i686-pc-linux-gnu/libxml2/2.9.2/include/libxml2"
-MODULE_VERSION="xml2-2.9.2"
-
